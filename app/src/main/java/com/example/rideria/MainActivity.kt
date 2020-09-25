6c821063b44d41dcef81f6a1af12a81b247c7483
abdullahbilalawan <mabilal.bese19seecs@seecs.edu.pk>
2020-09-25 20:30:55 +0500

    info activity added

--- app/src/main/java/com/example/rideria/MainActivity.kt
+++ app/src/main/java/com/example/rideria/MainActivity.kt
@@ -1,12 +1,19 @@
 
 package com.example.rideria
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Switch
+import com.google.firebase.database.FirebaseDatabase
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
      var usertypeswitch: Switch? = null
+     val name = UUID.randomUUID().toString()
+
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,4 +23,41 @@ class MainActivity : AppCompatActivity() {
 
         usertypeswitch = findViewById(R.id.switch1)
     }
+
+
+    fun goClicked(view: View){
+
+        if (usertypeswitch?.isChecked!!){
+
+            FirebaseDatabase.getInstance().getReference().child("Users").child("driver"+name).child("id").setValue(name)
+
+
+            val intent  = Intent(applicationContext,InfoActivity::class.java)
+
+            intent.putExtra("id",name)
+            startActivity(intent)
+
+
+
+
+        }
+
+        else if(!(usertypeswitch?.isChecked!!)){
+
+
+            FirebaseDatabase.getInstance().getReference().child("Users").child("Rider"+name).child("id").setValue(name)
+
+
+            val intent  = Intent(applicationContext,InfoActivity::class.java)
+
+            intent.putExtra("id",name)
+            startActivity(intent)
+
+
+
+
+        }
+
+    }
+
 }
